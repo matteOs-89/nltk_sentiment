9f8f4de5002fd5dc2a206fe791c01f7e82def1c0
Eseosa Jesuorobo <111536571+matteOs-89@users.noreply.github.com>
2023-02-03 00:36:23 +0000

    Update sentiment_A_NLTK.py

--- sentiment_A_NLTK.py
+++ sentiment_A_NLTK.py
@@ -1,49 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[40]:
-
 
 import nltk
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-
-# In[2]:
-
-
 nltk.download("vader_lexicon")
-
-
-# In[10]:
-
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 
-
-# For this task we will use NLTK framework to implyment Sentiment Analysis on Amazon review dataset. 
-# 
-# NLTK provides tools that enables us to carry out Sentiment Analysis in an efficient manner. Tools such as Vader's SentimentIntensityAnalyzer is available and we will explore it further in this project.
-
-# In[4]:
-
+"""
+For this task we will use NLTK framework to implyment Sentiment Analysis on Amazon review dataset. 
+NLTK provides tools that enables us to carry out Sentiment Analysis in an efficient manner. 
+Tools such as Vader's SentimentIntensityAnalyzer is available and we will explore it further in this project.
+"""
 
 sid = SentimentIntensityAnalyzer()
 
-
-# In[12]:
-
-
 df = pd.read_csv("/Users/eseosa/Desktop/NLP/UPDATED_NLP_COURSE/TextFiles/amazonreviews.tsv", sep="\t")
 
-
-# In[14]:
-
-
 print(df.head())
-
-
-# In[17]:
 
 
 df["label"].value_counts()
@@ -52,14 +26,10 @@ df["label"].value_counts()
 # pos    4903
 
 
-# In[20]:
-
-
 df.dropna(inplace=True)
 
 
-# In[25]:
-
+""" Check and return index of space columns on our dataset"""
 
 space_index = []
 
@@ -71,14 +41,7 @@ for idx,label,review in df.itertuples():
             
 space_index
 
-
-# In[27]:
-
-
 print(df.iloc[0:6]["review"])
-
-
-# In[31]:
 
 
 for i in range(6):
@@ -86,26 +49,14 @@ for i in range(6):
     print(sid.polarity_scores(df.iloc[i]["review"]))
 
 
-# In[34]:
-
-
 df["scores"] = df["review"].apply(lambda review: sid.polarity_scores(review))
+
 df["compound"] = df["scores"].apply(lambda idx: idx["compound"] )
-
-
-# In[38]:
-
 
 df["sentiment"] = df["compound"].apply(lambda x: "pos" if x >=0 else "neg")
 
 
-# In[42]:
-
-
 print(df)
-
-
-# In[53]:
 
 
 print(accuracy_score(df['label'], df["sentiment"]))
@@ -123,14 +74,26 @@ language barriers
 
 print(classification_report(df['label'], df["sentiment"]))
 
+    """   
+    precision    recall  f1-score   support
 
-# In[45]:
+         neg       0.86      0.51      0.64      5097
+         pos       0.64      0.91      0.75      4903
 
+   micro avg       0.71      0.71      0.71     10000
+   macro avg       0.75      0.71      0.70     10000
+weighted avg       0.75      0.71      0.70     10000
+
+"""
 
 print(confusion_matrix(df['label'], df["sentiment"]))
 
-
-# In[54]:
+"""
+[[2623   2474]
+ 
+ [ 435   4468]]
+ 
+ """
 
 
 def sentiment_rating(comment):
@@ -148,19 +111,13 @@ def sentiment_rating(comment):
         print("Review is Negative")
 
 
-# In[59]:
-
-
 sentiment_rating('poor service')
 
-
-# In[56]:
-
+"""Neutral Review"""
 
 sentiment_rating('great service')
 
-
-# In[ ]:
+"""Positive Review"""
 
 
 
